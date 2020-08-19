e5ee6122df99b2ce05ace44c76dd37c12d52cae0
Arthur Torres <arthur.torres@parkmobile.io>
2020-08-19 10:53:05 -0400

    first commit

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(input)
+	fmt.Println(output)
+}
---
+++ values.go
@@ -0,0 +1,6 @@
+package main
+
+const (
+	input  = "2\n7\n4 3 7 8 6 2 1\n4\n1 4 3 2"
+	output = "3 7 4 8 2 6 1\n1 4 2 3"
+)
