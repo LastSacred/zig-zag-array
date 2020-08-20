1e6456b34fac514d305cf82bd28ce911949990fd
Arthur Torres <arthur.torres@parkmobile.io>
2020-08-20 10:46:57 -0400

    MVP

--- src/main.go
+++ src/main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
 
 func main() {
-	input := os.Args[1]
 	testCases := formatInput(input)
 	rawOutput := zigZagTestCases(testCases)
 	output := formatOutput(rawOutput)
